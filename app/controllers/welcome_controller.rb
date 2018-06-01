95b628968cb54a2cd9854afaf53818f1db823f3f
paramireze <paramireze@gmail.com>
2018-06-01 11:44:58 -0400

    added welcome page with basic rest call

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+
   def index
+    
+    require 'rest-client'
+
+    asdf = RestClient.get 'http://hackathon.siim.org/fhir/Patient/siimjoe', { apiKey: '0619a72e-bbf4-4f85-a2b4-c6c2fd2d9cb7' }
+
+    @asdf = JSON.parse asdf
+
   end
 end
